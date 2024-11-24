684c16cc631721366af87a85f981581fe1a4b698
sulincix <parduscix@yandex.ru>
2024-11-24 09:15:45 +0000

    fix missing login for minimal profile

--- profiles/minimal.sh
+++ profiles/minimal.sh
@@ -2,5 +2,8 @@
 rc-update add udhcpc
 rc-update add hostname
 rc-update add devfs
+if [ ! -f /usr/bin/login ] ; then
+    ln -s /bin/busybox /usr/bin/login
+fi
 chmod u+s /usr/bin/su || true
 exit 0
