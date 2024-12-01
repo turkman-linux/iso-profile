46d3e691b0c0d20012246b795f0c3493cc7fc9fa
sulincix <parduscix@yandex.ru>
2024-12-01 00:22:52 +0000

    pipewire removed from x11 profile

--- profiles/x11.sh
+++ profiles/x11.sh
@@ -2,7 +2,7 @@
 # X11 install
 ymp repo --update --allow-oem --ignore-gpg
 ymp it xinit xorg-server xterm freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem --jobs=1
-ymp it elogind libtool shadow pipewire wireplumber fuse fuse2 --no-emerge --allow-oem --jobs=1
+ymp it elogind libtool shadow fuse fuse2 --no-emerge --allow-oem --jobs=1
 # enable services
 rc-update add elogind
 rc-update add devfs
